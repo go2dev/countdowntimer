b01ddf62a774ca31c087db75121d9862c45e0180
Chris Edson <christopher.edson@gmail.com>
2022-05-01 13:21:22 +0100

    Add click mechanic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { Box, Flex, Heading } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 import { intervalToDuration } from 'date-fns';
+
+import photo from 'public/photo.jpeg';
 
 const endDate = new Date(Date.parse('26 May 2022 19:00:00'));
 
@@ -10,8 +13,12 @@ const getIntervalToDuration = () =>
     end: endDate,
   });
 
+const colours = ['red', 'blue', 'orange', 'green'];
+
 export default function Home(): JSX.Element {
   const [intervalToDur, setIntervalToDur] = useState(getIntervalToDuration());
+
+  const [clickCount, setClickCount] = useState(0);
 
   const [countDown, setCountDown] = useState(
     endDate.getTime() - new Date().getTime()
@@ -28,6 +35,10 @@ export default function Home(): JSX.Element {
     return () => clearInterval(interval);
   }, []);
 
+  const onClick = () => {
+    setClickCount(clickCount + 1);
+  };
+
   return (
     <Flex
       w="full"
@@ -38,18 +49,24 @@ export default function Home(): JSX.Element {
       flex={1}
     >
       <Box width={{ base: 'full', md: '750px' }} textAlign={{ base: 'center' }}>
-        <Heading
-          color="red"
-          fontSize={{ base: '2xl', md: '5xl', lg: '8xl', xl: '16xl' }}
-        >
-          {`${intervalToDur.days}:${intervalToDur.hours}:${intervalToDur.minutes}:${intervalToDur.seconds}:`}
-          {countDown
-            .toString()
-            .slice(
-              countDown.toString().length - 3,
-              countDown.toString().length
-            )}
-        </Heading>
+        {clickCount < 10 ? (
+          <button onClick={onClick}>
+            <Heading
+              color={colours[clickCount % colours.length]}
+              fontSize={{ base: '2xl', md: '5xl', lg: '8xl', xl: '16xl' }}
+            >
+              {`${intervalToDur.days}:${intervalToDur.hours}:${intervalToDur.minutes}:${intervalToDur.seconds}:`}
+              {countDown
+                .toString()
+                .slice(
+                  countDown.toString().length - 3,
+                  countDown.toString().length
+                )}
+            </Heading>
+          </button>
+        ) : (
+          <Image src={photo} />
+        )}
       </Box>
     </Flex>
   );
